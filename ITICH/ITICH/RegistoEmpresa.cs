3f1641b438b84032b100080513e0f3ae6b913eca
migascl <miguelleirosa@outlook.com>
2020-11-24 15:27:02 +0000

    registo empresa

---
+++ ITICH/ITICH/RegistoEmpresa.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITICH
+{
+    public partial class RegistoEmpresa : Form
+    {
+        public RegistoEmpresa()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox10_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RegistoParque_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
